732e40032849808ef0700961d57fcaf8998e5fdb
mattackard <mattackard@gmail.com>
2020-02-03 17:25:07 -0600

    added default values if environment variables are not present

--- cmd/logger/logger.go
+++ cmd/logger/logger.go
@@ -7,17 +7,34 @@ import (
 	"github.com/mattackard/project-1/pkg/logutil"
 )
 
-var logPort = os.Getenv("LOGPORT")
-var logAddress = os.Getenv("LOGGERNAME")
 var dnsPort = os.Getenv("DNSPORT")
 var dnsName = os.Getenv("DNSNAME")
+var dnsAddr string
+
+var logPort = os.Getenv("LOGPORT")
+var logName = os.Getenv("LOGGERNAME")
+
+func init() {
+	//set the default dns address if none are specified in the environment
+	if dnsName == "" || dnsPort == "" {
+		dnsAddr = "dns:6060"
+	} else {
+		dnsAddr = dnsName + ":" + dnsPort
+	}
+
+	//same as above but for logger address
+	if logName == "" || logPort == "" {
+		logName = "logger"
+		logPort = "6060"
+	}
+}
 
 func main() {
 	logFile := logutil.OpenLogFile("./logs")
 	defer logFile.Close()
 
 	//send messages to log file and dns to record startup
-	dnsutil.Ping(dnsName+":"+dnsPort, "logger")
+	dnsutil.Ping(dnsAddr, logName)
 
-	logutil.CreateLogServerAndListen(logAddress, logPort, logFile)
+	logutil.CreateLogServerAndListen(logName, logPort, logFile)
 }
