2a69c28f09df745a90b1055a6116dd57ecfe0688
mattackard <mattackard@gmail.com>
2020-02-02 22:22:37 -0600

    started function to retrieve ip from dns server

--- pkg/dnsutil/dnsutil.go
+++ pkg/dnsutil/dnsutil.go
@@ -1,8 +1,8 @@
 package dnsutil
 
 import (
+	"bytes"
 	"fmt"
-	"log"
 	"net"
 )
 
@@ -25,11 +25,36 @@ func Ping(address string, serviceName string) net.Addr {
 //GetMyIP returns the caller's ip address by sending a blank request to google's DNS server
 //and retrieving the local address from the response
 func GetMyIP() net.Addr {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatalln(err)
+	var conn net.Conn
+	var err error
+	for {
+		conn, err = net.Dial("udp", "8.8.8.8:80")
+		if err == nil {
+			break
+		}
 	}
 	defer conn.Close()
 	ip := conn.LocalAddr()
 	return ip
+}
+
+//GetServiceIP queries the dns server for a currently running service and returns the IP address
+func GetServiceIP(dnsAddr string, serviceName string) string {
+	var conn net.Conn
+	var err error
+	for {
+		conn, err = net.Dial("tcp", dnsAddr)
+		if err == nil {
+			break
+		}
+	}
+	defer conn.Close()
+	conn.Write([]byte(serviceName))
+
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+
+	//trim te null characters from the buffer and convert to string
+	bufferText := string(bytes.Trim(buffer, "\x00"))
+	return bufferText
 }
