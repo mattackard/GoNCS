a6894b3af024be91115504cc447c5e21b10b7819
mattackard <mattackard@gmail.com>
2020-02-04 15:21:13 -0600

    added implementation of procstat package to get container stats

--- cmd/dashboard/dashboard.go
+++ cmd/dashboard/dashboard.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/mattackard/project-1/pkg/dnsutil"
 	"github.com/mattackard/project-1/pkg/logutil"
+
+	procstat "github.com/guillermo/go.procstat"
 )
 
 var dnsAddr string
@@ -20,7 +23,22 @@ func main() {
 
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/dashboard/", http.StripPrefix("/dashboard/", fs))
+	http.HandleFunc("/stats", serverStats)
 
 	logutil.SendLog(logAddr, false, []string{"Dashboard started at " + noPort}, logFile, "Dashboard")
 	http.ListenAndServe(":80", nil)
+}
+
+func serverStats(w http.ResponseWriter, r *http.Request) {
+	myStats := getStats()
+	fmt.Println(myStats)
+}
+
+func getStats() *procstat.Stat {
+	myStats := procstat.Stat{Pid: os.Getpid()}
+	err := myStats.Update()
+	if err != nil {
+		logutil.SendLog(logAddr, true, []string{err.Error()}, logFile, "Dashboard")
+	}
+	return &myStats
 }
